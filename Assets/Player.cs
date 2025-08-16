4805416349916e26e5023906f069b4af8079cf05
Stas Khmylko <metallstas@mail.ru>
2025-08-16 11:36:40 +0300

    added animations idle and run

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
 
     [SerializeField] private float moveSpeed = 5.0f;
@@ -11,21 +13,31 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
         handleInput();
-        handleMovment();   
+        handleMovment();
+        handleAnimations();
        
+    }
+
+    private void handleAnimations()
+    {
+        bool isMoving = rb.linearVelocityX != 0;
+        anim.SetBool("isMoving", isMoving);
     }
 
     private void handleInput()
     {
         xInput = Input.GetAxis("Horizontal");
 
-
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             Jump();
+        }
+      
     }
 
     private void Jump()
